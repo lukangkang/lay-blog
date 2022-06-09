dc0149cda1e5d5c86860b63c97bbd63adc5b5e39
fengchen <1157219723@qq.com>
2022-06-10 00:37:55 +0800

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout name="default">
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@import '@layui/layui-vue/lib/index.css';
+</style>
+
